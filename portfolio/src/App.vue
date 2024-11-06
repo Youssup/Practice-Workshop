dacae994ef78659eb90690f79af74b2241a73a57
John Doe <johndoe@example.com>
2024-11-06 15:54:39 -0500

    Basic setup

---
+++ portfolio/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+</style>
